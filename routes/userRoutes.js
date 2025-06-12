11c1d5aa14fd877ce35a1a8d14a1d7bf5e0f815b
“Dinesh98490” <kunwardinesh028@gmail.com>
2025-06-12 22:06:06 +0545

    user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userControllers');
 
+// routes of the users
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.get('/users', userController.getUsers);
