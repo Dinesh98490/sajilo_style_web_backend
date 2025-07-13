6d32499e887054395c51925a6eda789392c20446
“Dinesh98490” <kunwardinesh028@gmail.com>
2025-07-13 12:12:01 +0545

    minot changes

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// const app = require('./index');
-// const PORT = process.env.PORT || 5050;
-// const CONNECTION_STRING = process.env.MONGODB_URI;
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./index');
+const PORT = process.env.PORT || 5050;
+const CONNECTION_STRING = process.env.MONGODB_URI;
 
-// mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log('Sajilo Style running on server', PORT);
-//     });
-//     console.log('Mongodb connected');
-//   })
-//   .catch(err => {
-//     console.error('MongoDB connection error:', err);
-//   }); 
+mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Sajilo Style running on server', PORT);
+    });
+    console.log('Mongodb connected');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  }); 
 
-require("dotenv").config()
-const app = require("./index")
-
-const PORT = process.env.PORT
-app.listen(
-    PORT,
-    () => {
-        console.log("SajiloStyle Server running on 5050")
-    }
-)
